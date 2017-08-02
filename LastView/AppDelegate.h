6342f8807f412ae4b5bc920afc6a1c330dc97363
郑军 <junzheng3@creditease.cn>
2017-08-02 17:55:13 +0800

    Initial Commit

---
+++ LastView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LastView
+//
+//  Created by 郑军 on 2017/8/2.
+//  Copyright © 2017年 complany.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LastView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LastView
+//
+//  Created by 郑军 on 2017/8/2.
+//  Copyright © 2017年 complany.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
